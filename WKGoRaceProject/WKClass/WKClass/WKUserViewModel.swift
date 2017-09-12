9b2415fa2b6e63bfe229521c024d88b7539599dc
KangSky123 <417035863@qq.com>
2017-09-12 16:21:23 +0800

    1.0.0

---
+++ WKGoRaceProject/WKClass/WKClass/WKUserViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  WKUserViewModel.swift
+//  WKGoRaceProject
+//
+//  Created by vikan on 2017/9/12.
+//  Copyright © 2017年 vikan. All rights reserved.
+//
+
+import UIKit
+
+class WKUserViewModel: NSObject {
+
+}
